b8ebb5f53d6985f2b9a0ce0f5ce35248cd58a830
GIKONYO254 <87195989+GIKONYO254@users.noreply.github.com>
2024-11-27 04:20:34 +0300

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+
 
 dotenv.config();
 
@@ -20,3 +22,6 @@ const app = express();
 app.listen(8000, () => {    
     console.log('Server is running on port 8000!');
 }); 
+
+
+app.use('/api/user', userRoutes);   
