f71a68499c335f46c76a2d017469446506bbdde6
GIKONYO254 <87195989+GIKONYO254@users.noreply.github.com>
2024-12-03 21:22:06 +0300

    Add functionality to sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,55 +1,116 @@
-import { TextInput, Label, Button } from 'flowbite-react';
-import { Link } from 'react-router-dom';
-import React from 'react'
+import { Alert, Spinner, Button, Label, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
-  return (
-    <div className='min-h-screen mt-20'> 
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>'
-        {/* left side */}
-        <div className='flex-1'>
-        <Link
-          to="/" 
-          className=' font-bold dark:text-white text-4xl'  >
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'> Gikonyo's </span>
-            Blog
-        </Link>
-        <p className='text-sm mt-5'>
-          Welcome to the Ecodeed blog. Here you will find the latest news, updates and tips on how to make the most of your Ecodeed account.
-        </p>
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('All fields are required');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/SignIn');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className=''>
+          <Link to ='/' className='font-bold dark:text-white text-4xl'>
+          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+            Ecodeed
+          </span>
+          Blog
+          </Link>
+          <p className='text-sm mt-5'>
+            This is the Ecodeed Blog. Get you environmental news and updates here.
+          </p>
         </div>
 
-        {/* right side */}
-
+        {/* right */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value="Your username" />
-              <TextInput type='text' placeholder='Username' id='username' />
-              
+              <Label value='Your username' />
+              <TextInput
+                type='text'
+                placeholder='Username'
+                id='username'
+                onChange={handleChange}
+              />
             </div>
-            <div >
-              <Label value="Your email" />
-              <TextInput type='text' placeholder='Email' id='email' />
-             
+            <div>
+              <Label value='Your email' />
+              <TextInput
+                type='email'
+                placeholder='Email'
+                id='email'
+                onChange={handleChange}
+              />
             </div>
-            <div >
-              <Label value="Your password" />
-              <TextInput type='text' placeholder='Password' id='password' />
-             
+            <div>
+              <Label value='Your password' />
+              <TextInput
+                type='password'
+                placeholder='Password'
+                id='password'
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone='purpleToPink' type="submit" >
-              Sign Up 
-            </Button>   
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'> Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
+            </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Have an account? </span>
-            <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
-
+            <span>Have an account?</span>
+            <Link to='/SignIn' className='text-blue-500'>
+              Sign In
+            </Link>
           </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
