675ba9cf6e0b39f8a83a4b6d66333c30d6a1bff7
GIKONYO254 <87195989+GIKONYO254@users.noreply.github.com>
2024-12-20 05:12:37 +0300

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import { getposts } from '../controllers/post.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createImageBitmap)
+router.get('/getposts', getpost)
 
 
 export default router;
