ee6cc70de428507c5e84ac146b7f2cc9b856f04f
GIKONYO254 <87195989+GIKONYO254@users.noreply.github.com>
2024-12-21 10:14:17 +0300

    add user management functionality to dashboard with user list and delete option

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Sidebar } from "flowbite-react";
-import { HiUser, HiArrowSmRight, HiDocumentText } from "react-icons/hi";
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
@@ -51,15 +51,26 @@ export default function DashSidebar() {
             </Sidebar.Item>
           </Link>
           {currentUser.isAdmin && (
-            <Link to="/dashboard?tab=posts">
-              <Sidebar.Item
-                active={tab === "posts"}
-                icon={HiDocumentText}
-                as="div"
-              >
-                Posts
-              </Sidebar.Item>
-            </Link>
+            <>
+              <Link to="/dashboard?tab=posts">
+                <Sidebar.Item
+                  active={tab === "posts"}
+                  icon={HiDocumentText}
+                  as="div"
+                >
+                  Posts
+                </Sidebar.Item>
+              </Link>
+              <Link to="/dashboard?tab=users">
+                <Sidebar.Item
+                  active={tab === "users"}
+                  icon={HiOutlineUserGroup}
+                  as="div"
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+            </>
           )}
 
           <Sidebar.Item
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashProfile from '../components/DashProfile';
 import DashSidebar from '../components/DashSidebar';
+import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
 
 
 const Dashboard = () => {
@@ -27,6 +29,8 @@ const Dashboard = () => {
       {tab === 'profile' && <DashProfile />}
       {/* Posts */}
       {tab === 'posts' && <DashPosts />}
+      {/* Users */}
+      {tab === 'users' && <DashUsers />}
     </div>
   );
 };
