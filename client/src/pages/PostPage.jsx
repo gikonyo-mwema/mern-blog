730aec1e5235676a36d48c980dc81cb233656f4b
GIKONYO254 <87195989+GIKONYO254@users.noreply.github.com>
2024-12-22 15:30:12 +0300

    add call to action component and integrate it into the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -51,6 +51,12 @@ export default function PostPage() {
         </div>
 
         <div className='p-3 max-w-2xl mx-auto w-full post-content'dangerouslySetInnerHTML={{ __html: post && post.content }}></div>
+        <div className='max-w-4xl mx-auto w-full'>
+            <CallToAction/>
+        </div>
+
+
         </main>
+
 }
    
