394091bb324e032657bdfcf51462dc6b625efb81
GIKONYO254 <87195989+GIKONYO254@users.noreply.github.com>
2025-01-20 07:12:29 +0300

    refactor: improve layout in Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,22 @@
-import { Link } from 'react-router-dom';
-import CallToAction from '../components/CallToAction';
-import { useEffect, useState } from 'react';
-import PostCard from '../components/PostCard';
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
 
-
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch('/api/post/getPosts');
+        const response = await fetch("/api/post/getPosts");
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
         setPosts(data.posts);
       } catch (error) {
-        console.error('Failed to fetch posts:', error);
+        console.error("Failed to fetch posts:", error);
         return null;
       }
     };
@@ -25,42 +24,42 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
-        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
-        <p className='text-gray-500 text-xs sm:text-sm'>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
           Here you'll find a variety of articles and tutorials on topics such as
           web development, software engineering, and programming languages.
         </p>
+
         <Link
-    to='/search'
-    className='px-6 py-3 bg-white text-teal-600 font-semibold rounded-full hover:bg-gray-100 transition'
-  >
-    Discover Posts
-  </Link>
-     
-     
+          to="/search"
+          className="px-4 py-2 bg-white text-teal-600 font-semibold text-sm rounded-full hover:bg-gray-100 transition max-w-max"
+        >
+          Discover Posts
+        </Link>
 
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
-        {posts && posts.length > 0 && (
-          <div className='flex flex-col gap-6'>
-            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
-            <div className='flex flex-wrap gap-4'>
-              {posts.map((post) => (
-                <PostCard key={post._id} post={post} />
-              ))}
+        <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+          {posts && posts.length > 0 && (
+            <div className="flex flex-col gap-6">
+              <h2 className="text-2xl font-semibold text-center">
+                Recent Posts
+              </h2>
+              <div className="flex flex-wrap gap-4">
+                {posts.map((post) => (
+                  <PostCard key={post._id} post={post} />
+                ))}
+              </div>
+              <Link
+                to={"/search"}
+                className="text-lg text-teal-500 hover:underline text-center"
+              >
+                View all posts
+              </Link>
             </div>
-            <Link
-              to={'/search'}
-              className='text-lg text-teal-500 hover:underline text-center'
-            >
-              View all posts
-            </Link>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-
-      </div>
-      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
         <CallToAction />
       </div>
     </div>
