84f93e339e114a8a82fe868c0c4062ad6f71f883
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-01-27 03:52:57 +0300

    refactor: update default post image URL to a new source

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -19,7 +19,7 @@ const postSchema = new mongoose.Schema(
     image: {
       type: String,
       default:
-        'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
+        'https://www.africaodyssey.com/site/odyssey-image-proxy/park/laikipia-safaris-tailor-made-safari-holidays-in-kenya=402978-320.jpg',
     },
     category: {
       type: String,
