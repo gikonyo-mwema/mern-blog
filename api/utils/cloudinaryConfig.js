99cab8ef07771a0e80114cea4d0737636bdec211
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-01-27 03:49:22 +0300

    refactor: update Cloudinary configuration validation and clean up code

--- api/utils/cloudinaryConfig.js
+++ api/utils/cloudinaryConfig.js
@@ -1,27 +1,17 @@
 import { v2 as cloudinary } from 'cloudinary';
 import 'dotenv/config';
 
-// Validate required environment variables
+// Validate Cloudinary environment variables
 if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
-    throw new Error('Cloudinary environment variables are not properly set in .env file.');
+  throw new Error('Cloudinary environment variables are not properly set in .env file.');
 }
 
+// Cloudinary configuration
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-
-// Test Cloudinary setup
-cloudinary.api.ping((error, result) => {
-    if (error) {
-        console.error('Cloudinary setup error:', error);
-    } else {
-        console.log('Cloudinary setup successful:', result);
-    }
-});
-
 
 export default cloudinary;
 
