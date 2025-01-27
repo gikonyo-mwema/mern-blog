c056ed818fb672cdf56e6936af9aecdeb243936e
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-01-27 03:49:14 +0300

    refactor: streamline comment retrieval process and optimize performance

--- client/src/components/DashboardComponent.jsx
+++ client/src/components/DashboardComponent.jsx
@@ -6,26 +6,25 @@ import {
   HiAnnotation,
   HiArrowNarrowUp,
   HiDocumentText,
-  HiOutlineUserGroup,  
-  HiOutlineChat
+  HiOutlineUserGroup,
 } from "react-icons/hi";
 
 export default function DashboardComponent() {
   const [users, setUsers] = useState([]);
   const [comments, setComments] = useState([]);
   const [posts, setPosts] = useState([]);
-  const [totalUsers, setTotalUsers] = useState([]);
-  const [totalPosts, setTotalPosts] = useState([]);
-  const [totalComments, setTotalComments] = useState([]);
-  const [lastMonthUsers, setLastMonthUsers] = useState([]);
-  const [lastMonthPosts, setLastMonthPosts] = useState([]);
-  const [lastMonthComments, setLastMonthComments] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [totalComments, setTotalComments] = useState(0);
+  const [lastMonthUsers, setLastMonthUsers] = useState(0);
+  const [lastMonthPosts, setLastMonthPosts] = useState(0);
+  const [lastMonthComments, setLastMonthComments] = useState(0);
   const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await fetch("/api/user/getUsers?limit=5");
+        const res = await fetch("/api/user/getusers?limit=5");
         const data = await res.json();
         if (res.ok) {
           setUsers(data.users);
@@ -33,13 +32,13 @@ export default function DashboardComponent() {
           setLastMonthUsers(data.lastMonthUsers);
         }
       } catch (error) {
-        console.log(error);
+        console.error(error.message);
       }
     };
 
     const fetchPosts = async () => {
       try {
-        const res = await fetch("/api/user/getPosts?limit=5");
+        const res = await fetch("/api/user/getposts?limit=5");
         const data = await res.json();
         if (res.ok) {
           setPosts(data.posts);
@@ -47,13 +46,13 @@ export default function DashboardComponent() {
           setLastMonthPosts(data.lastMonthPosts);
         }
       } catch (error) {
-        console.log(error);
+        console.error(error.message);
       }
     };
 
     const fetchComments = async () => {
       try {
-        const res = await fetch("/api/comment/getComments?limit=5");
+        const res = await fetch("/api/comment/getcomments?limit=5");
         const data = await res.json();
         if (res.ok) {
           setComments(data.comments);
@@ -61,11 +60,11 @@ export default function DashboardComponent() {
           setLastMonthComments(data.lastMonthComments);
         }
       } catch (error) {
-        console.log(error);
+        console.error(error.message);
       }
     };
 
-    if (currentUser.isAdmin) {
+    if (currentUser?.isAdmin) {
       fetchUsers();
       fetchPosts();
       fetchComments();
@@ -73,65 +72,72 @@ export default function DashboardComponent() {
   }, [currentUser]);
 
   return (
-    <div>
-      <div className="p-3 md:mx-auto">
-        <div className="flex-wrap flex gap-4 justify-center">
-          <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
-            <div className="flex justify-between">
+    <div className="p-3 md:mx-auto">
+      {/* Overview Cards */}
+      <div className="flex-wrap flex gap-4 justify-center">
+        {/* Total Users Card */}
+        <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
+          <div className="flex justify-between">
+            <div>
               <h3 className="text-gray-500 text-md uppercase">Total Users</h3>
               <p className="text-2xl">{totalUsers}</p>
             </div>
             <HiOutlineUserGroup className="bg-teal-600 text-white rounded-full text-5xl p-3 shadow-lg" />
-            <div className="flex gap-2 text-sm">
-              <span className="text-green-500 flex items-center">
-                <HiArrowNarrowUp />
-                {lastMonthUsers}
-              </span>
-              <div className="text-gray-500">Last month</div>
-            </div>
           </div>
-          <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
-            <div className="flex justify-between">
-              <h3 className="text-gray-500 text-md uppercase">
-                Total Comments
-              </h3>
+          <div className="flex gap-2 text-sm">
+            <span className="text-green-500 flex items-center">
+              <HiArrowNarrowUp />
+              {lastMonthUsers}
+            </span>
+            <div className="text-gray-500">Last month</div>
+          </div>
+        </div>
+
+        {/* Total Comments Card */}
+        <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
+          <div className="flex justify-between">
+            <div>
+              <h3 className="text-gray-500 text-md uppercase">Total Comments</h3>
               <p className="text-2xl">{totalComments}</p>
             </div>
-            <HiOutlineChat className="bg-teal-600 text-white rounded-full text-5xl p-3 shadow-lg" />
-            <div className="flex gap-2 text-sm">
-              <span className="text-indigo-500 flex items-center">
-                <HiAnnotation />
-                {lastMonthComments}
-              </span>
-              <div className="text-gray-500">Last month</div>
-            </div>
+            <HiAnnotation className="bg-indigo-600 text-white rounded-full text-5xl p-3 shadow-lg" />
           </div>
-          <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
-            <div className="flex justify-between">
+          <div className="flex gap-2 text-sm">
+            <span className="text-green-500 flex items-center">
+              <HiArrowNarrowUp />
+              {lastMonthComments}
+            </span>
+            <div className="text-gray-500">Last month</div>
+          </div>
+        </div>
+
+        {/* Total Posts Card */}
+        <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
+          <div className="flex justify-between">
+            <div>
               <h3 className="text-gray-500 text-md uppercase">Total Posts</h3>
               <p className="text-2xl">{totalPosts}</p>
             </div>
-            <HiOutlineUserGroup className="bg-lime-600 text-white rounded-full text-5xl p-3 shadow-lg" />
-            <div className="flex gap-2 text-sm">
-              <span className="text-green-500 flex items-center">
-                <HiDocumentText />
-                {lastMonthPosts}
-              </span>
-              <div className="text-gray-500">Last month</div>
-            </div>
+            <HiDocumentText className="bg-lime-600 text-white rounded-full text-5xl p-3 shadow-lg" />
+          </div>
+          <div className="flex gap-2 text-sm">
+            <span className="text-green-500 flex items-center">
+              <HiArrowNarrowUp />
+              {lastMonthPosts}
+            </span>
+            <div className="text-gray-500">Last month</div>
           </div>
         </div>
       </div>
+
+      {/* Recent Data Tables */}
       <div className="flex flex-wrap gap-4 py-3 mx-auto justify-center">
+        {/* Recent Users Table */}
         <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
           <div className="flex justify-between p-3 text-sm font-semibold">
             <h1 className="text-center p-2">Recent Users</h1>
-            <Button 
-              outline
-              gradientDuoTone="purpleToPink"
-              onClick={() => navigate("/dashboard?tab=users")}
-            >
-              See all
+            <Button outline gradientDuoTone="purpleToPink">
+              <Link to="/dashboard?tab=users">See all</Link>
             </Button>
           </div>
           <Table hoverable>
@@ -139,80 +145,82 @@ export default function DashboardComponent() {
               <Table.HeadCell>User Image</Table.HeadCell>
               <Table.HeadCell>Username</Table.HeadCell>
             </Table.Head>
-            {users &&
-              users.map((user) => (
-                <Table.Body key={user._id} className="divide-y">
-                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell>
-                      <img
-                        src={user.profilePicture}
-                        alt="user"
-                        className="w-10 h-10 rounded-full bg-gray-500"
-                      />
-                    </Table.Cell>
-                    <Table.Cell>{user.username}</Table.Cell>
-                  </Table.Row>
-                </Table.Body>
+            <Table.Body>
+              {users.map((user) => (
+                <Table.Row key={user._id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    <img
+                      src={user.profilePicture}
+                      alt="user"
+                      className="w-10 h-10 rounded-full bg-gray-500"
+                    />
+                  </Table.Cell>
+                  <Table.Cell>{user.username}</Table.Cell>
+                </Table.Row>
               ))}
+            </Table.Body>
           </Table>
         </div>
+
+        {/* Recent Comments Table */}
         <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
           <div className="flex justify-between p-3 text-sm font-semibold">
-            <h1 className="text-center p-2">Recent Comment</h1>
+            <h1 className="text-center p-2">Recent Comments</h1>
             <Button outline gradientDuoTone="purpleToPink">
-              <Link to={"/dashboard?tab=comments"}>See all</Link>
+              <Link to="/dashboard?tab=comments">See all</Link>
             </Button>
           </div>
           <Table hoverable>
             <Table.Head>
-              <Table.HeadCell>Comment content</Table.HeadCell>
+              <Table.HeadCell>Comment Content</Table.HeadCell>
               <Table.HeadCell>Likes</Table.HeadCell>
             </Table.Head>
-            {comments &&
-              comments.map((comment) => (
-                <Table.Body key={comment._id} className="divide-y">
-                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell>
-                      <p className="line-clamp-2">{comment.content}</p>
-                    </Table.Cell>
-                    <Table.Cell>{comment.NumbeOfLikes}</Table.Cell>
-                  </Table.Row>
-                </Table.Body>
+            <Table.Body>
+              {comments.map((comment) => (
+                <Table.Row key={comment._id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    <p className="line-clamp-2">{comment.content}</p>
+                  </Table.Cell>
+                  <Table.Cell>{comment.numbeOfLikes}</Table.Cell>
+                </Table.Row>
               ))}
+            </Table.Body>
           </Table>
         </div>
+
+        {/* Recent Posts Table */}
         <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
           <div className="flex justify-between p-3 text-sm font-semibold">
-            <h1 className="text-center p-2">Recent Post</h1>
+            <h1 className="text-center p-2">Recent Posts</h1>
             <Button outline gradientDuoTone="purpleToPink">
-              <Link to={"/dashboard?tab=post"}>See all</Link>
+              <Link to="/dashboard?tab=posts">See all</Link>
             </Button>
           </div>
           <Table hoverable>
             <Table.Head>
               <Table.HeadCell>Post Image</Table.HeadCell>
-              <Table.HeadCell>Username</Table.HeadCell>
+              <Table.HeadCell>Title</Table.HeadCell>
               <Table.HeadCell>Category</Table.HeadCell>
             </Table.Head>
-            {posts &&
-              posts.map((post) => (
-                <Table.Body key={post._id} className="divide-y">
-                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell>
-                      <img
-                        src={post.image}
-                        alt="user"
-                        className="w-14 h-10 rounded-md bg-gray-500"
-                      />
-                    </Table.Cell>
-                    <Table.Cell className="w-96">{post.title}</Table.Cell>
-                    <Table.Cell className="w-5">{post.category}</Table.Cell>
-                  </Table.Row>
-                </Table.Body>
+            <Table.Body>
+              {posts.map((post) => (
+                <Table.Row key={post._id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    <img
+                      src={post.image}
+                      alt="post"
+                      className="w-14 h-10 rounded-md bg-gray-500"
+                    />
+                  </Table.Cell>
+                  <Table.Cell>{post.title}</Table.Cell>
+                  <Table.Cell>{post.category}</Table.Cell>
+                </Table.Row>
               ))}
+            </Table.Body>
           </Table>
         </div>
       </div>
     </div>
   );
 }
+
