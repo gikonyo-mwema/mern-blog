35247a25a0ff619a75ffd739e6ddf248f808a3dd
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-01-27 03:46:24 +0300

    refactor: add image upload support to post creation and update routes

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import { create, deletePost, getPosts, updatePost } from '../controllers/post.controller.js';
+import { upload } from '../utils/upload.js'
 
 const router = express.Router();
 
-router.post('/create', verifyToken, create)
-router.get('/getposts', getPosts)
-router.delete('/deletepost/:postId/:userId', verifyToken, deletePost)
-router.put('/updatepost/:postId/:userId', verifyToken, updatePost)
-
+// Post-related routes
+router.post('/create', verifyToken, upload.single('image'), create);  // Create a new post with an optional image
+router.get('/getPosts', getPosts);  // Fetch all posts (supporting query params like limit/pagination)
+router.delete('/deletePost/:postId/:userId', verifyToken, deletePost);  // Delete a specific post
+router.put('/updatePost/:postId/:userId', verifyToken, upload.single('image'), updatePost);  // Update a post with an optional image
 
 export default router;
+
