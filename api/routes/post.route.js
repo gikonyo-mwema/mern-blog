6385e909e4386660e2df1aa9f8fa499d395824a6
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-04-17 21:07:33 +0300

    feat: add route to fetch trending posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, deletePost, getPosts, updatePost } from '../controllers/post.controller.js';
+import { 
+    create, 
+    deletePost, 
+    getPosts, 
+    updatePost, 
+    getTrendingPosts
+} from '../controllers/post.controller.js';
 import { upload } from '../utils/upload.js'; // Import the upload middleware
 
 const router = express.Router();
@@ -8,6 +14,7 @@ const router = express.Router();
 // Post-related routes
 router.post('/create', verifyToken, upload.single('image'), create);  // Create a new post with an optional image
 router.get('/getPosts', getPosts);  // Fetch all posts (supporting query params like limit/pagination)
+router.get('/posts/trending', getTrendingPosts); // Add this new route
 router.delete('/deletePost/:postId/:userId', verifyToken, deletePost);  // Delete a specific post
 router.put('/updatePost/:postId/:userId', verifyToken, upload.single('image'), updatePost);  // Update a post with an optional image
 
