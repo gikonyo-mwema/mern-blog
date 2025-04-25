de500059dc68c04c734bd0f6b5fc795964e121da
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-04-25 16:45:37 +0300

    add routes for services and courses, including CRUD operations for services

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,12 @@ import PostPage from './pages/PostPage';
 import ScrollToTop from './components/ScrollToTop';
 import Search from './pages/Search';
 import ThemeProvider from './components/ThemeProvider';
+import Services from './pages/Services';
+import Courses from './pages/Courses';
+import ServiceDetails from './pages/ServiceDetails';
+import DashServices from './pages/DashServices';
+import CreateService from './pages/CreateService';
+import EditService from './pages/EditService';
 
 export default function App() {
   return (
@@ -25,6 +31,8 @@ export default function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/courses' element={<Courses />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/search' element={<Search />} />
@@ -34,13 +42,16 @@ export default function App() {
           <Route element={<OnlyAdminPrivateRoute />}>
             <Route path='/create-post' element={<CreatePost />} />
             <Route path='/update-post/:postId' element={<UpdatePost />} />
+            <Route path='/dashboard/services' element={<DashServices />} />
+            <Route path='/services/create' element={<CreateService />} />
+            <Route path='/services/edit/:serviceId' element={<EditService />} />
           </Route>
           <Route path='/projects' element={<Projects />} />
           <Route path='/post/:postSlug' element={<PostPage />} />
+          <Route path='/service/:serviceId' element={<ServiceDetails />} />
         </Routes>
         <Footer />
       </ThemeProvider>
     </Router>
   );
 }
-
