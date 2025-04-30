0da19e5863b4133916cd2168915ab501ea948207
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-04-30 06:30:31 +0300

    Refactor OnlyAdminPrivateRoute component to enhance user authentication flow; implement location state handling for redirecting unauthorized users and improve code readability.

--- client/src/components/OnlyAdminPrivateRoute.jsx
+++ client/src/components/OnlyAdminPrivateRoute.jsx
@@ -1,11 +1,17 @@
 import { useSelector } from 'react-redux';
-import { Outlet, Navigate } from 'react-router-dom';
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
 
 export default function OnlyAdminPrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser && currentUser.isAdmin ? (
-    <Outlet />
-  ) : (
-    <Navigate to='/sign-in' />
-  );
+  const location = useLocation();
+
+  if (!currentUser) {
+    return <Navigate to='/sign-in' state={{ from: location }} replace />;
+  }
+
+  if (!currentUser.isAdmin) {
+    return <Navigate to='/unauthorized' replace />;
+  }
+
+  return <Outlet />;
 }
