79bca4a0d6bce34b575e73940d266bd0ba430b25
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-05-30 05:55:35 +0300

    feat: Update payment route to use verifyToken for authentication

--- api/routes/payment.route.js
+++ api/routes/payment.route.js
@@ -6,19 +6,19 @@ import {
   handleFlutterwaveWebhook,
   checkPaymentStatus
 } from '../controllers/payment.controller.js';
-import { verifyUser } from '../utils/verifyUser.js';
+import { verifyToken } from '../utils/verifyUser.js'; 
 
 const router = express.Router();
 
 // Initiate payments
-router.post('/pesapal/initiate', verifyUser, initiatePesaPalPayment);
-router.post('/flutterwave/initiate', verifyUser, initiateFlutterwavePayment);
+router.post('/pesapal/initiate', verifyToken , initiatePesaPalPayment);
+router.post('/flutterwave/initiate', verifyToken , initiateFlutterwavePayment);
 
 // Payment callbacks
 router.post('/pesapal/ipn', handlePesaPalIPN);
 router.post('/flutterwave/webhook', handleFlutterwaveWebhook);
 
 // Check payment status
-router.get('/status/:itemId', verifyUser, checkPaymentStatus);
+router.get('/status/:itemId', verifyToken, checkPaymentStatus);
 
 export default router;
