14b30eb17c31130e78d824b8083492c80ad15439
gikonyo-mwema <gikonyo.mwema@gmail.com>
2025-06-22 01:48:25 +0300

    feat: Update payment route to use token verification for payment processing

--- api/routes/payment.route.js
+++ api/routes/payment.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { verifyPayment } from '../controllers/payment.controller.js';
-import { verifyUser } from '../utils/verifyUser.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.post('/verify', verifyUser, verifyPayment);
+router.post('/verify', verifyToken, verifyPayment);
 
-export default router;
+export default router; 
